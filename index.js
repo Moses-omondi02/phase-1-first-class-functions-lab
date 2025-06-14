c3d2fbcd12885f9404cb030da91c764de564707a
erick moses <moseserickomondi@gmail.com>
2025-06-14 17:01:18 +0300

    made changes on index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+// return first two drivers function
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
+
+// return last two drivers function
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]; // selecting drivers array
+
+ 
+function createFareMultiplier(integer) {
+  return function(fare) {
+    return fare * integer;
+  };
+}// create fare multiplier higher-order function
+
+
+const fareDoubler = createFareMultiplier(2);// fare doubler function
+
+// fareTripler function
+const fareTripler = createFareMultiplier(3);
+
+// selectDifferentDrivers function
+function selectDifferentDrivers(drivers, driverSelector) {
+  return driverSelector(drivers);
+}
+
